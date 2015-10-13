a4e059d6eb5debf563553819688ecb4f4f71732a
Cindy Li <li.cindy98@gmail.com>
2015-10-13 12:52:50 -0400

    hardcoded users for group members

---
+++ users.sql
@@ -0,0 +1,6 @@
+create table users (username, password)
+
+insert into users values ("cindyli", "cindy")
+insert into users values ("franklinwang", "franklin")
+insert into users values ("jasonshin", "jason")
+insert into users values ("masaheromasuda", "masahero")
