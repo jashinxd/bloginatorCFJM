09c0165e374cb315e7148cbced03b619f4dc2e38
li-cindy <li.cindy98@gmail.com>
2015-10-13 20:56:49 -0400

    login route

--- apps.py
+++ apps.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+import utils
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_tamplate("login.html")
+    else:
+        uname = request.form['username']
+        pword = request.form['password']
+        button = request.form['button']
+        if button == "cancel":
+            return render_template("login.html")
+        if utils.authenticate(uname,pword): 
+            return render_template("home.html")
+        else:
+            if utils.validuname(uname):
+                error = "Username and password do not match."
+            else:
+                error = "Username not in our database."
+            return render_template("login.html", err = error)
+@app.route("/register")
+def register():
+    
