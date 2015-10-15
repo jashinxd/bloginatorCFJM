80326bf1c23e72395cdb8b91ef988135e33eed5f
Jason Shin <jasonshin98@gmail.com>
2015-10-15 13:09:34 -0400

    Finished update tables methods

--- users.sql
+++
@@ -1,6 +0,0 @@
-create table users (username, password)
-
-insert into users values ("cindyli", "cindy")
-insert into users values ("franklinwang", "franklin")
-insert into users values ("jasonshin", "jason")
-insert into users values ("masaheromasuda", "masahero")
