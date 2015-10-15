63dd313f4559f167c08a0fda496e36110113b5c2
Cindy Li <li.cindy98@gmail.com>
2015-10-15 12:49:40 -0400

    update

---
+++ utils.py
@@ -0,0 +1,22 @@
+import sqlite3, csv
+
+conn = sqlite3.connect("demo.db")
+c = conn.cursor()
+
+def csv_dict():
+    reader = csv.DictReader(open("users.csv"))
+    TEMPLATE = "username: %(username)s, password: %(password)s)"
+    for item in reader:
+        print TEMPLATE%(username)
+        
+def authenticate(uname,pword):
+    if uname == "cindyli" and pword == "cindy":
+        return True
+    if uname == "jasonshin" and pword == "jason":
+        return True
+    if uname == "franklinwang" and pword == "franklin":
+        return True
+    if uname == "masaheromasuda" and pword == "masahero":
+        return True
+    else:
+        return False
