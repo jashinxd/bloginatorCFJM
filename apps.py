fc13342d2ccc4e08d32bdd16e1ec45492b975444
Masa13 <masahero55@gmail.com>
2015-10-19 06:29:20 -0400

    Update apps.py

--- apps.py
+++ apps.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/home/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", state = "Login")
 
 @app.route("/login", methods=["GET","POST"])
 def login():
