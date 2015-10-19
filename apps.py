395deb650ba4d0f5caca383430727dd3853fdf8a
Masa13 <masahero55@gmail.com>
2015-10-19 06:40:51 -0400

    Update apps.py

--- apps.py
+++ apps.py
@@ -6,49 +6,49 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/home/")
 def index():
-    return render_template("home.html", state = "Login")
+    return render_template("home.html", link = "/login", state = "Login")
 
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("login.html", link = "/login", state = "Login")
     else:
         uname = request.form['username']
         pword = request.form['password']
         button = request.form['button']
         if button == "cancel":
-            return render_template("login.html")
+            return render_template("login.html", link = "/login", state = "Login")
         if utils.authenticate(uname,pword):
             session['user'] = uname
-                return redirect(url_for("home"))
+                return redirect(url_for("home", link = "/login", state = "Login"))
         else:
             if utils.validuname(uname):
                 error = "Username and password do not match."
-                return render_template("login.html", err = error)
+                return render_template("login.html", err = error, link = "/login", state = "Login")
             else:
                 error = "Username not in our database."
-                return render_template("login.html", err = error)
+                return render_template("login.html", err = error, link = "/login", state = "Login")
 
 @app.route("/logout")
 def logout():
     session['user'] = 0
     print (session['user'])
-    return redirect(url_for("log_in"))
+    return redirect(url_for("log_in", link = "/login", state = "Login"))
     
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", link = "/login", state = "Login")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", link = "/login", state = "Login")
 
 @app.route("/register", methods=["GET","POST"])
 def register(): 
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("register.html", link = "/login", state = "Login")
     else:
         uname = request.form['username']
         pword = request.form['password']
@@ -56,16 +56,16 @@ def register():
         age = request.form['age']
         gender = request.form['gender']
         if button == "cancel":
-            return render_template("register.html")
+            return render_template("register.html", link = "/login", state = "Login")
         if utils.validuname(uname):
             error = "Username already exists. Please try again."
             return render_template("register.html", err = error)
         if uname == "" or pword == "" or age == "" or gender == "":
             error = "All fields must be filled in. Please try again."
-            return render_template("register.html", err = error)
+            return render_template("register.html", err = error, link = "/login", state = "Login")
         else:
             utils.register(uname, pword, age, gender)
-            return render_template("login.html")
+            return render_template("login.html", link = "/login", state = "Login")
     
 if __name__ == "__main__":
     app.debug = True
